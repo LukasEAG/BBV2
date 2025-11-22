ed9ca547be67cdfc9ca39ed9cc77903c452ebd98
Lukas EAG <lukas@eagproduction.com>
2025-11-23 00:14:53 +0100

    Start adding js, contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,28 @@
+const body = document.querySelector('body')
+const contactDiv = document.querySelector('#contact')
+const contactBtn = document.querySelector('.contactBtn')
+const closeContact = document.querySelector('.close-contact')
 
+const contacHendler = () => {
+	contactDiv.classList.contains('active') ? contactDiv.classList.remove('active') : contactDiv.classList.add('active')
+}
+
+const stopScrolling = () => {
+	body.classList.contains('stop-scrolling')
+		? body.classList.remove('stop-scrolling')
+		: body.classList.add('stop-scrolling')
+}
+contactBtn.addEventListener('click', e => {
+	e.preventDefault()
+	contacHendler()
+	stopScrolling()
+})
+
+closeContact.addEventListener('click', e => {
+	e.preventDefault()
+	contacHendler()
+	stopScrolling()
+})
 
 const footerYear = document.querySelector('.footer__foot-year')
 const handleCurrentYear = () => {
