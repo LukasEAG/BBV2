b01c3ab93baea43ce7232d4cc7ce3be1466a8a3b
Lukas EAG <lukas@eagproduction.com>
2025-11-23 14:34:29 +0100

    Add script nav mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,33 @@
 const body = document.querySelector('body')
+const mobileNav = document.querySelector('.nav-menu__links')
+const btnOpenNav = document.querySelector('.nav-menu__mobile-open-btn')
+const mobileMenuBtns = document.querySelectorAll('.nav-menu__link')
 const contactDiv = document.querySelector('#contact')
 const contactBtn = document.querySelector('.contactBtn')
 const closeContact = document.querySelector('.close-contact')
 
+
+const navHendler = () => {
+	mobileNav.classList.contains('active') ? mobileNav.classList.remove('active') : mobileNav.classList.add('active')
+}
 const contacHendler = () => {
 	contactDiv.classList.contains('active') ? contactDiv.classList.remove('active') : contactDiv.classList.add('active')
 }
-
 const stopScrolling = () => {
 	body.classList.contains('stop-scrolling')
 		? body.classList.remove('stop-scrolling')
 		: body.classList.add('stop-scrolling')
 }
+
+btnOpenNav.addEventListener('click', e => {
+	e.preventDefault()
+	navHendler()
+})
+mobileMenuBtns.forEach( btn => {
+	btn.addEventListener('click', () => {
+		navHendler()
+	})
+})
 contactBtn.addEventListener('click', e => {
 	e.preventDefault()
 	contacHendler()
@@ -23,6 +39,7 @@ closeContact.addEventListener('click', e => {
 	contacHendler()
 	stopScrolling()
 })
+
 
 const footerYear = document.querySelector('.footer__foot-year')
 const handleCurrentYear = () => {
