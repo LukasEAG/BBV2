3f0888d547be6877bbba70b4f53584b167b798d8
Lukas EAG <lukas@eagproduction.com>
2026-05-05 19:26:04 +0200

    Update paths and img before deploye

--- src/js/main.v2.js
+++ src/js/main.v2.js
@@ -11,6 +11,7 @@ const contactDiv = document.querySelector('#contact')
 const btnCloseContact = document.querySelectorAll('[btnCloseContact]')
 const submitButton = document.querySelector('.email__submit')
 const homeBtn = document.querySelectorAll('[btnHome]')
+const isMobile = window.innerWidth <= 992
 
 const navBarBgHandler = () => {
 	if (window.scrollY > 10) {
@@ -42,8 +43,14 @@ const openContactHandler = () => {
 }
 
 const closeContactHandler = () => {
+	console.log('jest')
 	contactDiv.classList.remove('active')
 	body.classList.remove('menuActive')
+	if (isMobile) {
+		mobileNav.classList.remove('active')
+		contactDiv.classList.remove('active')
+		body.classList.remove('menuActive')
+	}
 }
 const isContactOpen = () => contactDiv.classList.contains('active')
 
@@ -51,16 +58,14 @@ menuBtns.forEach(btn => {
 	btn.addEventListener('click', e => {
 		const btnLink = btn.querySelector('a')
 		const href = btnLink ? btnLink.getAttribute('href') : null
-		const isMobile = window.innerWidth <= 992
 
 		if (href === '#contact') {
+			console.log('tak')
 			isContactOpen() ? closeContactHandler() : openContactHandler()
 			if (isMobile) navHandler()
 		} else {
-			if (isContactOpen()) {
-				closeContactHandler()
-			}
-			if (isMobile) navHandler()
+			console.log('nie')
+			closeContactHandler()
 		}
 	})
 })
@@ -105,6 +110,7 @@ const createSpanHendler = msg => {
 	regFormPopup.classList.add('active')
 }
 const blockFormHendler = msg => {
+	console.log('blcok span', msg)
 	formBlocked = true
 	const span = document.createElement('span')
 	span.classList.add('form-popup__container--msg')
@@ -127,7 +133,7 @@ if (registrationForm) {
 }
 const checkRegLimit = async e => {
 	try {
-		const res = await fetch('/api/checkLimit', {
+		const res = await fetch('/api/limitMailing', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 		})
@@ -154,7 +160,7 @@ const sendFormToBackend = async e => {
 	}
 	sendingProccede.classList.add('active')
 	try {
-		const res = await fetch('/api/newsletter', {
+		const res = await fetch('/api/mailWrite', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(data),
@@ -179,7 +185,8 @@ Do zobaczenia nad wodą! 🌊 #BliskoBrzegu #DoZobaczenia`)
 				})
 
 				lastErrors = errors
-			} else if (!json.allowed) {
+			} else if (json.allowed === false) {
+				console.log(json)
 				blockFormHendler(json.message)
 			} else if (json.error) {
 				console.log(json.error)
@@ -223,13 +230,13 @@ const unsubscribeNewsLetter = async e => {
 	const form = e.target
 	const data = {
 		email: form.querySelector('[name="userEmailUs"]')?.value || '',
-		bbfCode: form.querySelector('[name="bbfCode"]')?.value || '',
+		name: form.querySelector('[name="userNameUs"]')?.value || '',
 		checkbox: form.querySelector('input[name="checkboxUs"]:checked')?.value || '',
 	}
-	console.log(data);
+	console.log(data)
 	sendingProccede.classList.add('active')
 	try {
-		const res = await fetch('/api/unsubscribe', {
+		const res = await fetch('/api/unsubscribeMail', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -240,7 +247,7 @@ const unsubscribeNewsLetter = async e => {
 
 		if (res.ok) {
 			createSpanHendler(`
-Zostałeś usunięty z naszego newslettera`)
+Na twojego maila został wysłany link do wypisania się z newslettera`)
 
 			lastErrors = {}
 		} else {
@@ -262,6 +269,156 @@ Zostałeś usunięty z naszego newslettera`)
 		createSpanHendler('Błąd połączenia z serwerem, spróbuj ponownie')
 	}
 }
+let active = 0
+let interval
+let startX = 0
+let currentX = 0
+let isDragging = false
+
+let sliderInitialized = false
+
+let currentTrack = null
+let currentDots = null
+let sliderElement = null
+
+const lineUpSliderHendler = () => {
+	const lineUpSlider = document.querySelector('.line-up__cards-box')
+	if (!lineUpSlider) return
+
+	sliderElement = lineUpSlider
+
+	const slides = [...lineUpSlider.querySelectorAll('.line-up__cards')]
+
+	const track = document.createElement('div')
+	track.classList.add('line-up__track')
+
+	slides.forEach(slide => track.appendChild(slide))
+	lineUpSlider.appendChild(track)
+	currentTrack = track
+
+	const dotsNav = document.createElement('ul')
+	dotsNav.classList.add('line-up__dots')
+
+	slides.forEach((_, index) => {
+		const li = document.createElement('li')
+		if (index === 0) li.classList.add('active')
+
+		li.addEventListener('click', () => {
+			clearInterval(interval)
+			changeSlide(index)
+		})
+
+		dotsNav.appendChild(li)
+	})
+
+	lineUpSlider.appendChild(dotsNav)
+	currentDots = dotsNav
+
+	const dots = dotsNav.querySelectorAll('li')
+
+	function setSlidesPosition() {
+		slides.forEach(slide => {
+			slide.style.transform = `translateX(-${active * 100}%)`
+		})
+	}
+
+	function changeSlide(index) {
+		active = index
+		setSlidesPosition()
+
+		dots.forEach(dot => dot.classList.remove('active'))
+		dots[active].classList.add('active')
+	}
+	let moved = false
+
+	const touchStart = e => {
+		if (e.touches.length === 1) {
+			startX = e.touches[0].clientX
+			currentX = startX
+			isDragging = true
+			moved = false
+		}
+	}
+
+	const touchMove = e => {
+		if (!isDragging) return
+		currentX = e.touches[0].clientX
+		moved = true
+	}
+
+	const touchEnd = () => {
+		if (!isDragging) return
+		isDragging = false
+
+		if (!moved) return
+
+		const diffX = currentX - startX
+		const swipeThreshold = 40
+
+		if (diffX > swipeThreshold) {
+			let prev = active - 1
+			if (prev < 0) prev = slides.length - 1
+			changeSlide(prev)
+		} else if (diffX < -swipeThreshold) {
+			let next = active + 1
+			if (next >= slides.length) next = 0
+			changeSlide(next)
+		}
+	}
+
+	lineUpSlider.addEventListener('touchstart', touchStart)
+	lineUpSlider.addEventListener('touchmove', touchMove)
+	lineUpSlider.addEventListener('touchend', touchEnd)
+
+	lineUpSlider._touchStart = touchStart
+	lineUpSlider._touchMove = touchMove
+	lineUpSlider._touchEnd = touchEnd
+
+	setSlidesPosition()
+}
+
+const destroySlider = () => {
+	if (!sliderElement) return
+
+	sliderElement.removeEventListener('touchstart', sliderElement._touchStart)
+	sliderElement.removeEventListener('touchmove', sliderElement._touchMove)
+	sliderElement.removeEventListener('touchend', sliderElement._touchEnd)
+
+	if (currentTrack) {
+		const slides = [...currentTrack.querySelectorAll('.line-up__cards')]
+		slides.forEach(slide => {
+			slide.style.transform = ''
+			sliderElement.appendChild(slide)
+		})
+
+		currentTrack.remove()
+		currentTrack = null
+	}
+	if (currentDots) {
+		currentDots.remove()
+		currentDots = null
+	}
+	active = 0
+}
+
+const media = window.matchMedia('(max-width: 768px)')
+
+function handleSlider(e) {
+	if (e.matches) {
+		if (!sliderInitialized) {
+			lineUpSliderHendler()
+			sliderInitialized = true
+		}
+	} else {
+		if (sliderInitialized) {
+			destroySlider()
+			sliderInitialized = false
+		}
+	}
+}
+
+handleSlider(media)
+media.addEventListener('change', handleSlider)
 
 const footerYear = document.querySelector('.footer__foot-year')
 const handleCurrentYear = () => {
